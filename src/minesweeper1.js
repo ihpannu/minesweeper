ce21744042e093988c6d77fab4affc48cc87c182
Harman Pannu <ukpannu@gmail.com>
2017-11-18 12:27:54 -0800

    First const

--- src/minesweeper1.js
+++ src/minesweeper1.js
@@ -1,4 +1,11 @@
 const generatePlayerBoard = (numberOfRows, numberOfColums) () => {
-    
+    const board = [];
+
+    for (let rowsIndex  = 0; rowsIndex < numberOfRows; rowsIndex++) {
+        const row = [];
+        for (let columnsIndex = 0; columnsIndex < numberOfColums; columnsIndex++ ) {
+            row.push(' ');
+        }
+    }
 
 };
