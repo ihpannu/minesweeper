39fc89a23f16a22ed6f8a18e90cf071a62a3dc2f
Harman Pannu <ukpannu@gmail.com>
2017-11-20 01:16:50 -0800

    File name changed

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,75 +1,55 @@
-// const blankLine = '  |   |  ';
-// console.log(blankLine);
-// console.log(blankLine);
-// console.log(blankLine);
-//
-// const guessLine = '1 |   |  ';
-// const bombLine =  '  | B |  ';
-// console.log('This is what a board with a guess and a bomb on it would look like:');
-// console.log(guessLine);
-// console.log(bombLine);
-// console.log(blankLine);
-//
-// const getAverage = (num1, num2) => {
-//   const average = (num1 + num2) * 2;
-//   console.log(average);
-// };
-// getAverage(34, 45);
+// node minesweeper1.js
 
-// printBoard(board);
-// board[0][1] = '1';
-// board[2][2] = 'b';
-// printBoard(board);
+const generatePlayerBoard = (numberOfRows, numberOfColums) => {
+    const board = [];
 
-
-
-const exBoard = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
-
-const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
-  const board = [];
-  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
-    const row = [];
-    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
-      row.push(' ');
+    for (let rowsIndex  = 0; rowsIndex < numberOfRows; rowsIndex++) {
+        const row = [];
+        for (let columnsIndex = 0; columnsIndex < numberOfColums; columnsIndex++ ) {
+            row.push(null);
+        }
+        board.push(row);
     }
-    board.push(row);
-  }
-  return board;
+    return board;
 };
 
 const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
-  const board = [];
-  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
-    const row = [];
-    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
-      row.push(null);
-    }
-    board.push(row);
-  }
+    const board = [];
+    for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+        const row = [];
+        for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+            row.push(null);
+        }
+        board.push(row);
+    };
 
-  let numberOfBombsPlaced = 0;
-  while (numberOfBombsPlaced < numberOfBombs) {
-    const randomRowIndex = Math.floor(Math.Random() * numberOfRows);
-    const randomColumnIndex = Math.floor(Math.Random() * numberOfColumns);
-
+let numberOfBombsPlaced = 0;
+while (numberOfBombsPlaced < numberOfBombs) {
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    
     board[randomRowIndex][randomColumnIndex] = 'B';
-
+    
     numberOfBombsPlaced++;
-  }
 
-  return board;
+// The code in your while loop has the potential to place bombs on top of
+// already existing bombs. This will be fixed when you learn about control flow.
+    }
+
+    return board;
 };
 
-const printBoard = board => {
-console.log(board.map(row => row.join(' | ')).join('\n'));
+
+
+
+const printBoard = (board) => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
 };
 
+const playerBoard = generatePlayerBoard(3,4);
+const bombBoard = generateBombBoard(3, 4, 5);
 
-const playerBoard = generatePlayerBoard(3, 3);
-const bombBoard = generateBombBoard(3, 2);
-
+console.log('Player Board: ');
 printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
